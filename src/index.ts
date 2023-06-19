8ea4bb29d047fbfa520ad821da69ed6bf6e3f421
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-06-19 14:05:45 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,40 @@ import * as vscode from 'vscode'
 import { createSnippetString, registerInlineCompletionItemProvider } from '@vscode-use/utils'
 import Claude from 'anthropic-ai'
 
+const claude = new Claude('')
 export function activate() {
-  const claude = new Claude('')
   const cacheMap: any = new Map()
   const fn = throttle(update)
   let text = ''
   async function update() {
     try {
-      if (cacheMap.has(text))
+      if (cacheMap.has(text)) {
         return new vscode.InlineCompletionList([
-          new vscode.InlineCompletionItem(cacheMap.get(new vscode.InlineCompletionItem(text))),
+          new vscode.InlineCompletionItem(cacheMap.get(text)),
+          new vscode.InlineCompletionItem(cacheMap.get(text)),
         ])
+      }
 
-      const data = await claude.complete(`我需要你和copilot一样,帮我推测我接下来要写的代码,请只保留推断的结果\n代码:\n${text}`, {
-        model: '1.3-100k',
-      })
-      const snip = createSnippetString(data)
+      const data = await request(10, `假如你是copilotX,推断我接下来的代码,并只保留推断的结果\n代码:\n${text}`) as string
+      if (!data)
+        return []
+      const snip = createSnippetString(data.trim().replace(/\n+/g, '\n'))
       cacheMap.set(text, snip)
+
       return new vscode.InlineCompletionList([
+        new vscode.InlineCompletionItem(snip),
         new vscode.InlineCompletionItem(snip),
       ])
     }
     catch (e) {
-      
+      return []
     }
   }
   registerInlineCompletionItemProvider(
     async (document) => {
       text = document.getText().trim()
       if (!text)
-        return
+        return []
       return await fn() as any
     },
   )
@@ -53,4 +57,18 @@ function throttle(fn: () => void, ms = 500) {
       }, ms)
     })
   }
+}
+
+function request(max = 10, text: string) {
+  const array = new Array(max).fill(0)
+  return new Promise((resolve) => {
+    Promise.race(array.map(() => {
+      return new Promise((resolve) => {
+        claude.complete(`假如你是copilotX,推断我接下来的代码,并只保留推断的结果\n代码:\n${text}`, {
+          model: '1.3',
+        }).then(resolve)
+      })
+    }),
+    ).then(resolve)
+  })
 }
